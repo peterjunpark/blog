791be1e3788b5ee84442c03071019d5bf24a1964
Peter Park <peterjpark@pm.me>
2023-06-09 19:18:14 -0400

    Add basic controllers and views

---
+++ server.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const { create } = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config/connection.js');
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+const hbs = create({});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+async function start() {
+  await sequelize.sync({ force: true });
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+}
+
+start();
