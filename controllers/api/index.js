c23d9cb39bb376388601b7f4a093c3f551a13aec
Peter Park <peterjpark@pm.me>
2023-06-12 02:24:58 -0400

    Move login and logout routes to controllers/auth-routes.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,10 @@
 const apiRouter = require('express').Router();
+const authRouter = require('./auth-routes.js');
 const commentRouter = require('./comment-routes.js');
 const postRouter = require('./post-routes.js');
 const userRouter = require('./user-routes.js');
 
-apiRouter.get('/', (req, res) => {
-  res.json({ message: 'hi' });
-});
-
+apiRouter.use(authRouter);
 apiRouter.use('/comment', commentRouter);
 apiRouter.use('/post', postRouter);
 apiRouter.use('/user', userRouter);
