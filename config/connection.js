044f8632572bc3950645716cab882c6d779b01e1
Peter Park <peterjpark@pm.me>
2023-06-13 19:42:18 -0400

    Connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
+let sequelize;
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  }
-);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: '127.0.0.1',
+      dialect: 'mysql',
+    }
+  );
+}
 
 module.exports = sequelize;
